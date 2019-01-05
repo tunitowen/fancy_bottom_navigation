244a49f984543951c5031189c573dd6446516242
Anthony Owen <tony@owentech.co.uk>
2019-01-05 09:10:37 +0000

    Images for readme

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.teal,
+        primarySwatch: Colors.deepOrange,
 
       ),
       home: MyHomePage(),
