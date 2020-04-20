94b4c6fd7e3533bdc4d7b79032c97faa85d29d0c
Mohamed Sayed <25801517+msayed-net@users.noreply.github.com>
2020-04-21 00:03:13 +0200

    fix rtl app layout padding TEXT_OFF

--- lib/internal/tab_item.dart
+++ lib/internal/tab_item.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 const double ICON_OFF = -3;
 const double ICON_ON = 0;
-const double TEXT_OFF = 3;
+const double TEXT_OFF = 5;
 const double TEXT_ON = 1;
 const double ALPHA_OFF = 0;
 const double ALPHA_ON = 1;
