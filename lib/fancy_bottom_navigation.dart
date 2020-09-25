42c7e01c27c0d68148dbc536d3c9eafc64722438
Eimji <Eimji@users.noreply.github.com>
2020-09-25 22:16:16 +0200

    Replace deprecated overflow with clipBehavior: Clip.none

--- lib/fancy_bottom_navigation.dart
+++ lib/fancy_bottom_navigation.dart
@@ -117,7 +117,7 @@ class FancyBottomNavigationState extends State<FancyBottomNavigation>
   @override
   Widget build(BuildContext context) {
     return Stack(
-      overflow: Overflow.visible,
+      clipBehavior: Clip.none,
       alignment: Alignment.bottomCenter,
       children: <Widget>[
         Container(
